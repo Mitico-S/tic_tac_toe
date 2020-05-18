82da1411f4f00e05813e3859f238c8494f2fd5de
João Pedro Alves <jotalmeida007@hotmail.com>
2020-05-18 12:14:53 -0300

    second commit

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './TicTacToe.css';
 
-function TicTacTo() {
-  return (
-    <span>Jogo da velha</span>
+
+function TicTacToe() {
+  const emptyBoard = Array(9).fill("");
+
+  const [board, setBoard] = useState(emptyBoard);
+  const [currentPlayer, setCurrentPlayer] = useState("O");
+  const [winner, setWinner] = useState();
+
+  const handleCellClick = (index) => {
+    if(board[index] !== "") return (window.alert("Posição ocupada"));
+
+    setBoard(board.map((item, itemIndex) => itemIndex === index ? currentPlayer : item)
+    ); 
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X"); 
+  }
+
+  
+
+  const checkWinner = () => {
+    const possibleWaysToWin = [
+      [board[0], board[1], board [2]],
+      [board[3], board[4], board [5]],
+      [board[6], board[7], board [8]],
+      [board[0], board[3], board [6]],
+      [board[1], board[4], board [7]],
+      [board[2], board[5], board [8]],
+      [board[0], board[4], board [8]],
+      [board[2], board[4], board [6]],
+    ];
+
+    possibleWaysToWin.forEach(cells => {
+        if (cells.every(cell => cell === "O")) setWinner("X")
+        if (cells.every(cell => cell === "X")) setWinner("O")O
+    });
+  }
+  useEffect(checkWinner, [board]);
+  
+
+    return (
+  <main>  
+    <h1 className="title">Jogo da velha</h1> 
+    <div className="board">
+      {board.map((item, index) => (
+        <div 
+          key={index}
+          className={`cell ${item}`}
+          onClick={() => handleCellClick(index)}
+        >
+          {item}
+        </div>
+      ))}
+    </div>
+  </main>
   );
 }
 
